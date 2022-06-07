0b144116b7912f22daf1e4328f15f5fc42304a3a
RealForce1024 <337940626@qq.com>
2022-06-07 17:33:23 +0800

    feat: init spring boot 3.x helloworld

---
+++ src/main/java/com/realforce1024/restspec/RestApiCommonSpecApplication.java
@@ -0,0 +1,13 @@
+package com.realforce1024.restspec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApiCommonSpecApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestApiCommonSpecApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/realforce1024/restspec/RestApiCommonSpecApplicationTests.java
@@ -0,0 +1,13 @@
+package com.realforce1024.restspec;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApiCommonSpecApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
