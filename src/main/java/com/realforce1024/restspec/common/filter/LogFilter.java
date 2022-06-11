ca900d28bd1dad3194b5d4d2cae77a20cd08a107
RealForce1024 <337940626@qq.com>
2022-06-11 17:39:14 +0800

    feat: 6.1.6-MDC集成log4j2日志记录requestId等变量字段

--- src/main/java/com/realforce1024/restspec/common/filter/LogFilter.java
+++ src/main/java/com/realforce1024/restspec/common/filter/LogFilter.java
@@ -4,7 +4,9 @@ package com.realforce1024.restspec.common.filter;
 import com.realforce1024.restspec.common.request.RequestModel;
 import jakarta.servlet.*;
 import jakarta.servlet.annotation.WebFilter;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
+import org.slf4j.MDC;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
@@ -30,8 +32,18 @@ public class LogFilter implements Filter {
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
         String reqId = UUID.randomUUID().toString().replace("-", "");
         Long startTime = Instant.now().toEpochMilli();
-        log.info("startTime: {}", startTime);
-        log.info("filter reqId: {}", reqId);
+        String requestURI = ((HttpServletRequest) request).getRequestURI();
+        String httpMethod = ((HttpServletRequest) request).getMethod();
+        String clientIp = request.getRemoteAddr();
+
+        MDC.put("req_id", reqId);
+        MDC.put("req_uri", requestURI);
+        MDC.put("http_method", httpMethod);
+        MDC.put("client_ip", clientIp);
+
+
+        log.info("reqId: {},startTime: {},reqUri:{}", reqId, startTime, requestURI);
+
         RequestModel requestModel = new RequestModel();
         requestModel.setReqId(reqId);
         requestModel.setStartTime(startTime);
@@ -39,6 +51,7 @@ public class LogFilter implements Filter {
         RequestModel.setRequestModel(requestModel);
         chain.doFilter(request, response);
         RequestModel.remove();
+        MDC.clear();
     }
 
     @Override
