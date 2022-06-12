60c010b4a116ceada82a53e380685dd302d107d2
RealForce1024 <337940626@qq.com>
2022-06-12 22:31:10 +0800

    feat: 7.5-404特殊静态资源配置处理

--- src/main/java/com/realforce1024/restspec/common/config/WebConfig.java
+++ src/main/java/com/realforce1024/restspec/common/config/WebConfig.java
@@ -4,6 +4,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.StringHttpMessageConverter;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
@@ -12,12 +14,27 @@ import java.util.List;
  * @author 编程燃风 RealForce1024
  */
 @Slf4j
-// @Configuration
-public class WebConfig implements WebMvcConfigurer {
+@Configuration
+public class WebConfig extends WebMvcConfigurationSupport {//implements WebMvcConfigurer {
+    // @Override
+    // public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+    //     log.info("移除StringHttpMessageConverter");
+    //     converters.removeIf(httpMessageConverter -> httpMessageConverter instanceof StringHttpMessageConverter);
+    //     WebMvcConfigurer.super.configureMessageConverters(converters);
+    // }
+
     @Override
-    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
-        log.info("移除StringHttpMessageConverter");
-        converters.removeIf(httpMessageConverter -> httpMessageConverter instanceof StringHttpMessageConverter);
-        WebMvcConfigurer.super.configureMessageConverters(converters);
+    protected void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/swagger-ui.html")
+                .addResourceLocations("classpath:/META-INF/resources/", "/static", "/public");
+
+        registry.addResourceHandler("/webjars/**")
+                .addResourceLocations("classpath:/META-INF/resources/webjars/");
+        
+        registry.addResourceHandler("/static/**")
+                .addResourceLocations("classpath:/static/");
+
+        super.addResourceHandlers(registry);
     }
+
 }
