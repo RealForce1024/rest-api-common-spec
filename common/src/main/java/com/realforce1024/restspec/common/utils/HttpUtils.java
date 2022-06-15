3b10f67830bc6efde030d89b50775f6ab76f1b5d
RealForce1024 <337940626@qq.com>
2022-06-15 10:26:23 +0800

    feat: 9.1-抽取为公共包并使用

--- common/src/main/java/com/realforce1024/restspec/common/utils/HttpUtils.java
+++ common/src/main/java/com/realforce1024/restspec/common/utils/HttpUtils.java
@@ -1,12 +1,9 @@
 package com.realforce1024.restspec.common.utils;
 
-import jakarta.servlet.ServletInputStream;
 import jakarta.servlet.ServletRequest;
 import lombok.SneakyThrows;
 
 import java.io.BufferedReader;
-import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.util.HashMap;
 import java.util.Map;
 
